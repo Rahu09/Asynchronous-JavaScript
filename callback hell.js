ecc51af8a004572f6ea6e568e1982faafd3432dc
priya <priyaraj00987@gmail.com>
2023-04-20 05:59:17 +0530

    callback hell added

---
+++ callback hell.js
@@ -0,0 +1,60 @@
+/*
+What are Callbacks in JavaScript?
+When you nest a function inside another function as an argument, that's called a callback.
+
+Why do we use callbacks?
+When doing a complex task, we break that task down into smaller steps. To help us establish a relationship between these steps according to time (optional) and order, we use callbacks.
+*/
+let stocks = {
+    Fruits : ["strawberry", "grapes", "banana", "apple"],
+    liquid : ["water", "ice"],
+    holder : ["cone", "cup", "stick"],
+    toppings : ["chocolate", "peanuts"],
+ };
+
+let order = (fruit_name, call_production) =>{
+
+  setTimeout(function(){
+
+    console.log(`${stocks.Fruits[fruit_name]} was selected`)
+
+// Order placed. Call production to start
+   call_production();
+  },2000)
+};
+
+let production = () =>{
+
+  setTimeout(()=>{
+    console.log("production has started")
+
+    setTimeout(()=>{
+      console.log("The fruit has been chopped")
+
+      setTimeout(()=>{
+        console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} Added`)
+
+        setTimeout(()=>{
+          console.log("start the machine")
+
+          setTimeout(()=>{
+            console.log(`Ice cream placed on ${stocks.holder[1]}`)
+
+            setTimeout(()=>{
+              console.log(`${stocks.toppings[0]} as toppings`)
+
+              setTimeout(()=>{
+                console.log("serve Ice cream")
+              },2000)
+            },3000)
+          },2000)
+        },1000)
+      },1000)
+    },2000)
+  },0000)
+
+};
+// This is called callback hell. It looks something like this (remember that code just above?)
+
+// Trigger 👇
+order(0, production);
